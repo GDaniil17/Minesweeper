8c75a541aed6f584eacbfc6b623c1a238185b25d
GDaniil17 <32881349+GDaniil17@users.noreply.github.com>
2020-12-12 02:38:54 +0300

    Fully working version.

---
+++ Сапёр/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Сапёр
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.richTextBox3);
+            this.panel1.Controls.Add(this.richTextBox4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(950, 950);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(83, 435);
+            this.richTextBox3.MaxLength = 2;
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(300, 80);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.TabStop = false;
+            this.richTextBox3.Text = "Side:";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox4.Location = new System.Drawing.Point(420, 435);
+            this.richTextBox4.MaxLength = 3;
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(447, 80);
+            this.richTextBox4.TabIndex = 5;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(82, 308);
+            this.richTextBox2.MaxLength = 2;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(300, 80);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.TabStop = false;
+            this.richTextBox2.Text = "Bombs:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(419, 308);
+            this.richTextBox1.MaxLength = 3;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(447, 80);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(82, 563);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(784, 119);
+            this.button1.TabIndex = 4;
+            this.button1.TabStop = false;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(966, 995);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Enter()
+        {
+
+        }
+
+        private void Btn()
+        {
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(966, 31);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            //this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(90, 28);
+            this.toolStripSplitButton1.Text = "Functions";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.Controls.Add(this.toolStrip1);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(335, 92);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(146, 76);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "0";
+            this.panel1.Controls.Add(this.textBox1);
+            textBox1.SetBounds(0, 0, 180, 90);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Сапёр/Form1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Сапёр
+{
+    public partial class Form1 : Form
+    {
+
+        int count_seconds = 0;
+        int[,] directions = { { -1, 0 }, { -1, -1, }, { -1, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { 0, 1 } };
+        int size = 12;
+        public int count_bombs;
+        int count = 0;
+        Random rand = new Random();
+        Bomb_or_Cell[,] field;
+        int s = 0;
+        bool game_just_started = false;
+
+
+        void ShowBombs()
+        {
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    field[i, j].btn.Text = field[i, j].status.Text;
+                    field[i, j].btn.Font = new Font("Times New Roman", 44, field[i, j].btn.Font.Style | FontStyle.Bold);
+                    if (field[i, j].isBomb == 1)
+                    {
+                        field[i, j].btn.BackColor = Color.Gold;
+                    }
+                    else
+                    {
+                        field[i, j].btn.BackColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        bool Exist(int x, int y)
+        {
+            return (x > -1 && x < size && y > -1 && y < size);
+        }
+
+        void Reveal(Bomb_or_Cell cell)
+        {
+
+            if (game_just_started)
+            {
+                s = 0;
+                game_just_started = false;
+                Console.WriteLine("IMMMMMMMMM Working!!!!!!!!!");
+                timer1.Start();
+            }
+            Console.WriteLine("Clicked {0} {1}", cell.X, cell.Y);
+            cell.isOpened = true;
+            cell.status.Font = new Font("Times New Roman", 44, cell.status.Font.Style | FontStyle.Bold);
+            cell.status.Show();
+            cell.btn.Hide();
+            count += 1;
+            cell.btn.Hide();
+            int new_x;
+            int new_y;
+            Bomb_or_Cell t;
+
+            for (int i = 0; i < 8; i++)
+            {
+                new_x = cell.X + directions[i, 0];
+                new_y = cell.Y + directions[i, 1];
+                if (Exist(new_x, new_y) && field[new_x, new_y].isBomb == 0 && field[new_x, new_y].isOpened == false && field[new_x, new_y].status.Text == "0")
+                {
+                    t = field[new_x, new_y];
+                    OnCellClicked(t);
+                }
+            }
+
+        }
+        void OnCellClicked(Bomb_or_Cell cell)
+        {
+            if (cell.isBomb == 1)
+            {
+                textBox1.Text = "0";
+                timer1.Stop();
+                    textBox1.Hide();
+                ShowBombs();
+                MessageBox.Show("Игра окончена!");
+                toolStripSplitButton1.HideDropDown();
+                panel1.Hide();
+                toolStrip1.Hide();
+
+                InitializeComponent();
+            }
+            else
+            {
+                //Console.WriteLine("count {0}", count);
+                Reveal(cell);
+                //Console.WriteLine("count {0}", count);
+                if (size * size - count == count_bombs)
+                {
+                    textBox1.Text = "0";
+                    timer1.Stop();
+                    textBox1.Hide();
+                    MessageBox.Show(String.Format("You won! \n In {0} second(-s) and haven't launched {1} bomb(-s)", s.ToString(), count_bombs));
+                    toolStripSplitButton1.HideDropDown();
+                    panel1.Hide();
+                    toolStrip1.Hide();
+
+                    InitializeComponent();
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //InitField(size);
+        }
+
+
+
+        void InitField(int _bombs)
+        {
+            s = 0;
+            game_just_started = true;
+            panel1.Size = new System.Drawing.Size(90*size, 90*(size+1));
+            panel1.MinimumSize = new System.Drawing.Size(90 * size, 90 * (size+1));
+            panel1.MaximumSize = new System.Drawing.Size(90 * size, 90 * (size+1));
+            count_bombs = Math.Min(_bombs, size * size);
+            //Console.WriteLine("///{0} {1}", _bombs, size);
+            count = 0;
+            field = new Bomb_or_Cell[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    field[i, j] = new Bomb_or_Cell();
+                    field[i, j].X = i;
+                    field[i, j].Y = j;
+                    field[i, j].isOpened = false;
+                    field[i, j].isBomb = 0;
+                    field[i, j].status = new Label();
+                    field[i, j].status.Font = new Font("Times New Roman", 44, field[i, j].status.Font.Style | FontStyle.Bold);
+                    field[i, j].btn = new Button();
+
+
+                }
+            }
+
+            for (int i = 0; i < Math.Min(count_bombs, size*size); i++)
+            {
+                int x = rand.Next(0, size);
+                int y = rand.Next(0, size);
+                while (field[x, y].isBomb == 1)
+                {
+                    x = rand.Next(0, size);
+                    y = rand.Next(0, size);
+
+                }
+                field[x, y].isBomb = 1;
+
+            }
+            for (int x = 0; x < size; ++x)
+            {
+                for (int y = 0; y < size; ++y)
+                {
+
+                    Label l = new Label();
+                    if (field[x, y].isBomb == 1)
+                    {
+                        l.Text = "☠";
+                    }
+                    else
+                    {
+                        l.Text = field[x, y].Risk.ToString();
+                    }
+                    l.SetBounds(90 * x, 90 * (y+1), 90, 90);
+                    l.Font = new Font("Times New Roman", 44, l.Font.Style | FontStyle.Bold);
+                    l.Parent = panel1;
+                    l.Hide();
+                    field[x, y].status = l;
+
+                    Bomb_or_Cell t = new Bomb_or_Cell();
+                    field[x, y].X = x;
+                    field[x, y].Y = y;
+                    field[x, y].isOpened = false;
+                    //field[x, y] = t;
+                    field[x, y].btn = new Button();
+                    field[x, y].btn.SetBounds(90 * x, 90 * (y+1), 90, 90);
+
+                    field[x, y].btn.Parent = panel1;
+                    t = field[x, y];
+
+                    field[x, y].btn.Click += (sender, e) => OnCellClicked(t);
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i > 0)
+                    {
+                        field[i, j].Risk += field[i - 1, j].isBomb;
+                        if (j < size - 1)
+                        {
+                            field[i, j].Risk += field[i - 1, j + 1].isBomb;
+                        }
+                        if (j > 0)
+                        {
+                            field[i, j].Risk += field[i - 1, j - 1].isBomb;
+                        }
+                    }
+                    if (i < size - 1)
+                    {
+                        field[i, j].Risk += field[i + 1, j].isBomb;
+                        if (j < size - 1)
+                        {
+                            field[i, j].Risk += field[i + 1, j + 1].isBomb;
+                        }
+                        if (j > 0)
+                        {
+                            field[i, j].Risk += field[i + 1, j - 1].isBomb;
+                        }
+                    }
+                    if (j < size - 1)
+                    {
+                        field[i, j].Risk += field[i, j + 1].isBomb;
+                    }
+                    if (j > 0)
+                    {
+                        field[i, j].Risk += field[i, j - 1].isBomb;
+                    }
+
+                    Label l = new Label();
+                    if (field[i, j].isBomb == 1)
+                    {
+                        l.Text = "☠";
+                    }
+                    else
+                    {
+                        l.Text = field[i, j].Risk.ToString();
+                    }
+                    l.SetBounds(90 * i, 90 * (j+1), 90, 90);
+                    l.Parent = panel1;
+                    l.Hide();
+                    field[i, j].status = l;
+
+
+
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripSplitButton1.HideDropDown();
+            panel1.Hide();
+            toolStrip1.Hide();
+            textBox1.Hide();
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = richTextBox1.Text;
+            bool tr = true;
+            string s2 = richTextBox4.Text;
+            bool tr2 = true;
+            if (s.Length != 0)
+            {
+                foreach (char i in s)
+                {
+                     if (Char.IsDigit(i) == false)
+                    {
+                        tr = false;
+                    }
+                }
+            }
+            if (s2.Length != 0)
+            {
+                foreach (char i in s2)
+                {
+                    if (Char.IsDigit(i) == false)
+                    {
+                        tr2 = false;
+                    }
+                }
+            }
+
+            if (tr2 && s.Length != 0)
+            {
+                size = Math.Max(2, Math.Min(Int32.Parse(s2), (Screen.PrimaryScreen.WorkingArea.Height-50)/100));
+            }
+            else
+            {
+                size = 2;
+            }
+
+            if (tr && s.Length != 0)
+            {
+                //InitializeComponent();
+                richTextBox1.Hide();
+                richTextBox2.Hide();
+                richTextBox3.Hide();
+                richTextBox4.Hide();
+                button1.Hide();
+                count_bombs = Math.Max(1, Math.Min(Int32.Parse(s), size*size));
+                InitField(count_bombs);
+                Btn();
+            }
+            else
+            {
+                //InitializeComponent();
+                richTextBox1.Hide();
+                richTextBox2.Hide();
+                richTextBox3.Hide();
+                richTextBox4.Hide();
+                button1.Hide();
+                count_bombs = Math.Max(1, Math.Min(5, size*size));
+                InitField(5);
+                Btn();
+            }
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            s += 1;
+            textBox1.Text = s.ToString();
+
+        }
+    }
+
+    public class Bomb_or_Cell
+    {
+        public sbyte isBomb;
+        public int Risk;
+        public bool isOpened;
+        public int X;
+        public int Y;
+        public Button btn;
+        public Label status;
+    }
+}
